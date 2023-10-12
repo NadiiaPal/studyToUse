8d8cbb89d650a9a3d99443fb9afeca75b32c1317
Nadiia <nadyapaliuha@gmail.com>
2023-10-12 23:30:39 +0300

    starting point

--- src/staticc/Car.java
+++ src/staticc/Car.java
@@ -4,6 +4,8 @@ public class Car {
     String nameOfModel;
     int yearProduction;
     boolean isBroken;
+    int speed;
+    int wheel;
     Car(String nameOfModel, int yearProduction, boolean isBroken){
         this.nameOfModel = nameOfModel;
         this.yearProduction = yearProduction;
