79b593c9f8b3d0e1f02a7506259f11b09a8dbb2c
Nadiia <nadyapaliuha@gmail.com>
2023-10-12 23:15:03 +0300

    adding a string into Car

--- src/staticc/Car.java
+++ src/staticc/Car.java
@@ -4,6 +4,7 @@ public class Car {
     String nameOfModel;
     int yearProduction;
     boolean isBroken;
+    String state= "Show what change";
     Car(String nameOfModel, int yearProduction, boolean isBroken){
         this.nameOfModel = nameOfModel;
         this.yearProduction = yearProduction;
