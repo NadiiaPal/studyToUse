85241f4a6089d96d6a071dd5a86530cab46d1583
Nadiia <nadyapaliuha@gmail.com>
2023-10-12 23:28:53 +0300

    del string, add speed and wheel

--- src/staticc/Car.java
+++ src/staticc/Car.java
@@ -4,7 +4,8 @@ public class Car {
     String nameOfModel;
     int yearProduction;
     boolean isBroken;
-    String state= "Show what change";
+    int speed;
+    int wheel;
     Car(String nameOfModel, int yearProduction, boolean isBroken){
         this.nameOfModel = nameOfModel;
         this.yearProduction = yearProduction;
